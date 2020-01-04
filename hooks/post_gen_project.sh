ec2e12aee7e1d059b3ac1c56bbc3e525ddfaa06a
Brian Pugh <bnp117@gmail.com>
2020-01-04 15:45:13 -0800

    Set remote repo on slug

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,3 +3,4 @@
 git init
 git add .
 git commit -a -m "Initial Cookiecutter Commit from github.com:BrianPugh/cookiecutter-esp-idf-component.git ( 0.1.0 )."
+git remote add origin {{cookiecutter.git_repo}}
