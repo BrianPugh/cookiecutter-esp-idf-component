32bca161f6b486db47e60af3a18da2c6ecbf466c
Brian Pugh <bnp117@gmail.com>
2020-01-04 15:22:38 -0800

    Bump version: 0.0.0 → 0.1.0

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,4 +2,4 @@
 
 git init
 git add .
-git commit -a -m "Initial Cookiecutter Commit from github.com:BrianPugh/cookiecutter-esp-idf-component.git ( 0.0.0 )."
+git commit -a -m "Initial Cookiecutter Commit from github.com:BrianPugh/cookiecutter-esp-idf-component.git ( 0.1.0 )."
