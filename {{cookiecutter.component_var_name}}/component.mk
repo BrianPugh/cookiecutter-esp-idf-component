5b52432a4b36cf3a9f85e86418bd8f2c1b7d2c95
Brian Pugh <bnp117@gmail.com>
2020-01-04 20:33:51 -0800

    Add COMPONENT_PRIV_INCLUDEDIRS to slug's component.mk

--- {{cookiecutter.component_var_name}}/component.mk
+++ {{cookiecutter.component_var_name}}/component.mk
@@ -6,4 +6,8 @@ COMPONENT_NAME := {{cookiecutter.component_var_name}}
 
 COMPONENT_SRCDIRS := src
 
+# Added to the include search path for all components in the project
 COMPONENT_ADD_INCLUDEDIRS := include
+
+# include search path for just this component
+#COMPONENT_PRIV_INCLUDEDIRS :=
